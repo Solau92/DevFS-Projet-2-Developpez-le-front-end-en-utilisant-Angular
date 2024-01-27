aa8b3997b46080a7cedbb916467fdeba35d5922d
Sophie LAURENT <sophielaurent92@gmail.com>
2024-01-27 11:11:33 +0100

    Nb of JOs and nb of countries in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,10 +14,30 @@ export class HomeComponent implements OnInit {
   //public olympics$: Observable<any> = of(null);
   public olympics$: Observable<Olympic[]> = of([]);
 
+  public numberOfJOs :number = 0;
+  public numberOfCourtries :number = 0;
+
+
   constructor(private olympicService: OlympicService) { }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
 
+    this.olympics$.subscribe(value => this.numberOfCourtries = value.length);
+    this.olympics$.subscribe(value => this.numberOfJOs = this.calculateNumberOfJOs(value));
+
   }
+
+  public calculateNumberOfJOs(value: Olympic[]) :number {
+
+    let jos = new Set<string>();
+
+    for (let i = 0 ; i < value.length ; i++) {
+      for(let j = 0 ; j < value[i].participations.length ; j++) {
+        jos.add(value[i].participations[j].city);
+      }
+    }
+    return jos.size;
+  }
+
 }
