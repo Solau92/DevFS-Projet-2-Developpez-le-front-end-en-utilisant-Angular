7ddd15db8bb7451b7d0c5cd4b0801b49141e3283
Sophie LAURENT <sophielaurent92@gmail.com>
2024-01-30 14:25:46 +0100

    Documentation and TODOs

--- src/app/core/models/Participation.ts
+++ src/app/core/models/Participation.ts
@@ -3,11 +3,9 @@
  * Represents a participation to an Olympic game
  */
 export class Participation {
-
     id!: number;
     year!: number;
     city!: string;
     medalsCount!: number;
     athleteCount!: number;
-
 } 
